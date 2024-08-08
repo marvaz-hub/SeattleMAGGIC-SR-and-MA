22801f2f6d4ec1c154ffe295b0b06b85a964178e
marvaz-hub <maria.vazquezmontes@phc.ox.ac.uk>
2024-08-08 11:58:26 +0100

    Update subg_MAGGIC_ACM_3yr_calib_HFtype.R
    Deleted personal directory path

--- subgroup analysis/subg_MAGGIC_ACM_3yr_calib_HFtype.R
+++ subgroup analysis/subg_MAGGIC_ACM_3yr_calib_HFtype.R
@@ -39,7 +39,7 @@ path2 = "subscripts/"
 #  source(paste0(path1, path2, "fn_forestplot_mlab.r"))
 # titplot <- "MAGGIC" #" 3-year ACM"
 # figfile <- "Fig5b_M_3yr_ACMapprox_cal_HFtype.jpg"
-# pathfig <- "C:/Users/mvazquezmontes/OneDrive - Nexus365/Documents/SHFM and MAGGIC/Analysis files/RCode/figures/"
+# pathfig <- # INSERT THE PATH WHERE YOU WANT TO SAVE THE FIGURES
 
 
 #################################################################################
