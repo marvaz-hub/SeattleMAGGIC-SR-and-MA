15b380aaec25e0a9dab169d83bcfe6773a64b1f1
marvaz-hub <maria.vazquezmontes@phc.ox.ac.uk>
2024-08-08 11:44:00 +0100

    Update overall_MAGGIC_ACMapprox_1yr_calib.R
    Deleted personal directory path

--- primary analysis/overall_MAGGIC_ACMapprox_1yr_calib.R
+++ primary analysis/overall_MAGGIC_ACMapprox_1yr_calib.R
@@ -39,7 +39,7 @@ source(paste0(path1, path2, "fn_forestplot_mlab.r"))
 source(paste0(path1, path2, "fn_forestplot_calib.R"))
 titplot <- "MAGGIC"
 figfile <- "Fig1b_M_1yr_ACMapprox_calib.png"
-pathfig <- "C:/Users/mvazquezmontes/OneDrive - Nexus365/Documents/SHFM and MAGGIC/Analysis files/RCode/figures/"
+pathfig <- # INSERT THE PATH WHERE YOU WANT TO SAVE THE FIGURES
 
 #################################################################################
 ### create the necessary dataframes for plotting and pooling ###
